bca7e973a4013188f0ea560c8720e86f4a46b9bd
oemercem.tabar <oemercem.tabar@studenti.unidp.it>
2024-11-03 19:46:33 +0100

    KVKK.API Files first commit

---
+++ KVKK.API/Startup.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KVKK.API.Models;
+using KVKK.API.Data;
+using KVKK.API.Services;
+
+namespace KVKK.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowAnyOrigin();
+                    });
+            });
+            var connectionString = Configuration["connectionStrings: KVKKDbConnectionString"];
+            services.AddDbContext<KvkkDbContext>();            
+            services.AddTransient<IKurumRepository, KurumRepository>();
+            services.AddTransient<IKullaniciRepository, KullaniciRepository>();
+            services.AddTransient<IBirimRepository, BirimRepository>();
+            services.AddTransient<IRolRepository, RolRepository>();
+            services.AddTransient<IKartvizitRepository, KartvizitRepository>();
+            services.AddTransient<IPaylasimRepository, PaylasimRepository>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "KVKK.API", Version = "v1" });
+            });
+           
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "KVKK.API v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("AllowAll");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
